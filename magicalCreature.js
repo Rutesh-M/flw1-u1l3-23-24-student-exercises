f3c5430f5bfb10c631e1336486c47fd7499acc90
Rutesh-M <124737800+Rutesh-M@users.noreply.github.com>
2023-10-18 22:20:02 +0000

    I worked in the javascript

--- magicalCreature.js
+++ magicalCreature.js
@@ -2,26 +2,35 @@
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
 
+let creatureName="Elf";
 
+let creatureName2="warlock";
 
+creatureName2=creatureName;
+console.log(creatureName2)//elf
+creatureName="dragon";
+console.log(creatureName)//dragon
+console.log(creatureName2)//dragon
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
+let creatureDescription="it lives in the forest";
 
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
 
-
+let isFriendly=true
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
-
+let visitorAge=Math.floor(Math.random()*60)+1
 
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
+let canVisit=visitorAge>15
 
 
 
